91635bb46ad35bf0217a7d672462048197e79835
Simsons, Janis <simsonsjanis@gmail.com>
2019-05-25 13:10:30 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexObjects
+{
+    public class Class1
+    {
+    }
+}
